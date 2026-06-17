ad9b443ee4ba927d5860a880cd4895bcd2608e23
muhammadalimurtadho05 <muhammadalimurtadho2209051@gmail.com>
2026-06-17 18:12:54 +0700

    fix: loading event add

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="h-10 bg-white border-t border-slate-200 flex items-center justify-between px-6 flex-shrink-0">
-    <span class="text-slate-400 text-xs">© 2025 SiAbsen · Sistem Absensi Digital</span>
-    <span class="text-slate-400 text-xs">v2.0.0 · Build 20250606</span>
+    <span class="text-slate-400 text-xs">© 2026 Smart Precence System - HIMATIF 2026 </span>
+    <span class="text-slate-400 text-xs">Departemen Riset dan Teknologi</span>
 </footer>
